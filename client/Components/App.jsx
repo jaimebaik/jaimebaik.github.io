4b8e0e7a5aeedbbc1f4a1cbe837828101aaf5c91
jaimebaik <jaimebaik5@gmail.com>
2021-02-04 10:22:39 -0800

    [add]boilerplate

---
+++ client/Components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/styles.css';
+
+function App() {
+
+  return (
+    <>
+      <div id='app'>
+        <div className='red'>my website</div>
+      </div>
+    </>
+  )
+}
+export default App;
